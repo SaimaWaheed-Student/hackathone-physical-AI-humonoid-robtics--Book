fe67b18c25452693eb46bb64701d5eba5bd34dd7
Saima <160252804+SaimaWaheed-Student@users.noreply.github.com>
2026-02-07 12:13:37 -0800

    Change baseUrl for Docusaurus configuration

--- my-book/docusaurus.config.ts
+++ my-book/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://saimawaheed-student.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/hackathone-physical-AI-humonoid-robtics--Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
